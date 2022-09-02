9776c8e33013b235f0e6dbc8d0ff4b9e5c95acad
Saurabh <awasthisaurabh0700@gmail.com>
2022-09-02 11:24:20 +0530

    App.js

---
+++ app.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', () => {
+    
+    var navi = document.querySelectorAll('.nav .element');
+    navi.forEach(elem => {
+        changeOrder(elem)
+    })
+    
+})
+
+function changeOrder(element){
+    let order = Math.floor((Math.random() * 6) + 1);
+    element.style = `order: ${order}`;
+    console.log(order);
+}
+
+const hiddenButton = document.querySelector('.displayNoneButton');
+const wave = document.querySelector('.wave');
+
+hiddenButton.addEventListener('click' , () => {
+  wave.style = 'animation-name: window';
+
+})
+
+
+function tap() {
+    let tap = document.querySelector(".more");
+    let show = tap.style.display?"":"block";
+    tap.style.display = `${show}`;
+  //   console.log(show);
+  }
